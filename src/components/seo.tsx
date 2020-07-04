a8e23062a2dab91e5b87943bd3120c66d2352609
Petar <petizad55@gmail.com>
2020-07-04 14:46:21 +0300

    Transitioning everything to typescript

--- src/components/seo.tsx
+++ src/components/seo.tsx
@@ -6,11 +6,17 @@
  */
 
 import React from "react"
-import PropTypes from "prop-types"
 import { Helmet } from "react-helmet"
 import { useStaticQuery, graphql } from "gatsby"
 
-function SEO({ description, lang, meta, title }) {
+type SEOData = {
+  description?: string,
+  lang?: string,
+  meta?: any[],
+  title?: string
+}
+
+const SEO: React.FC<SEOData> = ({ description, lang, meta = [], title }) => {
   const { site } = useStaticQuery(
     graphql`
       query {
@@ -76,13 +82,7 @@ SEO.defaultProps = {
   lang: `en`,
   meta: [],
   description: ``,
-}
-
-SEO.propTypes = {
-  description: PropTypes.string,
-  lang: PropTypes.string,
-  meta: PropTypes.arrayOf(PropTypes.object),
-  title: PropTypes.string.isRequired,
+  title: ''
 }
 
 export default SEO
