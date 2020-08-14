f12a22be0c4dda48ad0545a1731fe6b27b0f0995
Petar <petizad55@gmail.com>
2020-08-14 20:29:23 +0300

    Cought a bug when deploying, more info in ticket

--- src/components/elements/textBox.tsx
+++ src/components/elements/textBox.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react"
 import { faGithub } from "@fortawesome/free-brands-svg-icons"
 import { motion } from "framer-motion"
+import { useMedia } from "react-media"
 
 import { useTypedSelector } from "../../state/createStore"
 import { graphql, useStaticQuery } from "gatsby"
@@ -11,26 +12,8 @@ type Props = {
     current: carrosselState
 }
 
-function getWidth() {
-    return window.innerWidth
-}
-
-function useWindowWidth() {
-    const [width, setWidth] = useState(getWidth())
-
-    useEffect(() => {
-        function handleResize() {
-            setWidth(getWidth())
-        }
-
-        window.addEventListener('resize', handleResize)
-    }, [])
-
-    return width
-}
-
 const TextBox = () => {
-    const width = useWindowWidth()
+    const isLargeScreen = useMedia({query: "(min-width: 768px)"})
     const current = useTypedSelector(state => state.carrossel.current)
     const [state, setState] = useState({ width: 0, height: 0, text: { name: "", content: () => { return <div /> } } })
     const data = useStaticQuery(graphql`
@@ -61,8 +44,8 @@ const TextBox = () => {
         switch (current) {
             case "id-badge":
                 setState({
-                    width: width > 768 ? 1000 : 300,
-                    height: width > 768 ? 160 : 350,
+                    width: isLargeScreen ? 1000 : 300,
+                    height: isLargeScreen  ? 160 : 350,
                     text: {
                         name: "About me",
                         content: () => {
@@ -82,7 +65,7 @@ const TextBox = () => {
                 break;
             case "list-alt":
                 setState({
-                    width: width > 768 ? 1000 : 300,
+                    width: isLargeScreen ? 1000 : 300,
                     height: 200,
                     text: {
                         name: "My Repositories",
@@ -98,7 +81,7 @@ const TextBox = () => {
                 break;
             case "link":
                 setState({
-                    width: width > 768 ? 800 : 300,
+                    width: isLargeScreen ? 800 : 300,
                     height: 200,
                     text: {
                         name: "Links",
@@ -126,7 +109,7 @@ const TextBox = () => {
                 })
                 break;
         }
-    }, [current, width])
+    }, [current, isLargeScreen])
 
     return (
         <motion.div
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,10 +2,14 @@ import React from "react"
 
 import Layout from "../components/elements/layout"
 import SEO from "../components/seo"
+import TextBox from "../components/elements/textBox"
+import Name from "../components/elements/name"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
+    <Name />
+    <TextBox />
   </Layout>
 )
 
