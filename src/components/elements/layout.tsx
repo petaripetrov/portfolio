5085cd0f79d8fd773f631b40e18dbd69e454c760
Petar <petizad55@gmail.com>
2020-08-14 17:18:41 +0300

    background final touches

--- src/components/elements/layout.tsx
+++ src/components/elements/layout.tsx
@@ -5,7 +5,7 @@
  * See: https://www.gatsbyjs.org/docs/use-static-query/
  */
 
-import React, { CSSProperties } from "react"
+import React, { CSSProperties, useEffect } from "react"
 import { useStaticQuery, graphql } from "gatsby"
 import { motion, useAnimatedState } from 'framer-motion'
 
@@ -13,6 +13,35 @@ import '../../styles/global.css'
 
 type DataProps = {
   children: any
+}
+
+const Star: React.FC = () => {
+
+  const variant = {
+    hidden: {
+      left: `${Math.random() * 98}%`,
+      top: `${Math.random() * 98}%`,
+      transition: {
+        type: "spring",
+        delay: 0,
+        stiffness: 500,
+        damping: 60,
+        mass: 1
+      }
+    },
+    active: {
+      opacity: [1, 0, 1],
+      scale: [1, 0, 1],
+      transition: {
+        yoyo: Infinity,
+        repeatDelay: Math.random()
+      }
+    }
+  }
+
+  return (
+    <motion.div variants={variant} initial="hidden" style={{ position: 'absolute' }} animate="active" className="star"></motion.div>
+  )
 }
 
 const Layout: React.FC<DataProps> = ({ children }) => {
@@ -27,40 +56,8 @@ const Layout: React.FC<DataProps> = ({ children }) => {
   `)
   let items = []
 
-  let test = {
-    initial: {
-
-    },
-    animated: {
-      scale: [1, 0.8, 1],
-      opacity: [1, 0.5, 1],
-      transition: {
-        yoyo: Infinity,
-        repeatDelay: Math.random()
-      }
-    }
-  }
-
   for (let i = 0; i < 100; i++) {
-    let star =
-      <motion.div className="star-wrapper" animate={{
-        left: Math.random() * window.innerWidth - 10,
-        top: Math.random() * window.innerHeight - 10
-      }}
-        transition={{
-          type: "spring",
-          delay: 0,
-          stiffness: 500,
-          damping: 60,
-          mass: 1
-        }} >
-        <motion.div
-          className="star"
-          animate={{ opacity: [1, 0, 1], scale: [1, 0.2, 1] }} transition={{ yoyo: Infinity, repeatDelay: Math.random() * 20 }}
-        >
-        </motion.div>
-      </motion.div>
-    items.push(star)
+    items.push(<Star />)
   }
 
   return (
